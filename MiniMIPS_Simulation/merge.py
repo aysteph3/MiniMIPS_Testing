da2005feb5a9b32f5d0a309114299bdadc346326
aysteph3 <aysteph3@yahoo.com>
2017-03-26 01:06:11 +0200

    testbench update and python simulation scripts
    test bench update, reads input from file and writes output to generated
    file. call python script to generate random patterns and merge output

---
+++ MiniMIPS_Simulation/merge.py
@@ -0,0 +1,54 @@
+import sys
+# delete the first line of the output file
+def delete_line(num):
+  #num = 1
+
+  with open('generated_file\out.txt', 'r') as fr:          
+      lines = fr.readlines()
+      if num in range(len(lines)):
+        del lines[num-1]
+
+  with open('generated_file\out.txt', 'w') as fw:
+      fw.writelines(lines)
+
+
+# merge content of file, appending last column in each segment to the first
+def mergefile(filen):
+  #delete_line(1)
+  try:
+     f = open(filen,'r')
+  except Exception as e:
+    print "Could not copy file %s " % (filen)
+    print e
+    sys.exit(3)
+ 
+
+  lines = []
+
+  firstPass = True
+  counter = 0
+
+  for line in f:
+      try:
+          line = line.strip()
+          columns = line.split()
+          name = columns[3]
+
+          if firstPass:
+              lines.append(line)
+          else:
+              lines[counter] += ' ' + name
+          counter += 1
+
+      except IndexError:
+          counter = 0
+          firstPass = False
+
+  f.close()
+  out = open('generated_file\output.txt', 'w')
+  for line in lines:
+      out.write(line + "\n")
+  out.close()
+
+#filename = "out.txt"
+#mergefile(filename)
