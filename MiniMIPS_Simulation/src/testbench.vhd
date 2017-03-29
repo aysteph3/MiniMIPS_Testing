4f8d7a23f7a3e0ee44ee0fd0bf2bd36bf383f289
aysteph3 <aysteph3@yahoo.com>
2017-03-29 16:45:43 +0300

    fixed timing in testbench

--- MiniMIPS_Simulation/src/testbench.vhd
+++ MiniMIPS_Simulation/src/testbench.vhd
@@ -12,56 +12,56 @@
   end testbench;
 
   architecture behaviour of testbench is 
-  	-- component declaration 
-  		component alu is
-  			port(
-  					clock : in bus1;
-   					reset : in bus1;
-   					op1 : in bus32;            -- Operand 1
-   					op2 : in bus32;            -- Operand 2
-   					ctrl : in alu_ctrl_type;   -- Opearator control
+    -- component declaration 
+      component alu is
+        port(
+            clock : in bus1;
+            reset : in bus1;
+            op1 : in bus32;            -- Operand 1
+            op2 : in bus32;            -- Operand 2
+            ctrl : in alu_ctrl_type;   -- Opearator control
 
-   					res : out bus32;           -- The result is 32 bit long
-   					overflow : out bus1        -- Overflow of the result
-  				);
-  		end component;
+            res : out bus32;           -- The result is 32 bit long
+            overflow : out bus1        -- Overflow of the result
+          );
+      end component;
 
-  		signal clock 			    : std_logic := '0';
-  		signal reset 			    : std_logic := '1';
-  		signal op1 				    : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
-  		signal op2 				    : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
-  		signal ctrl 	        : std_logic_vector(27 downto 0) := "0000000000000000000000000000";
-  		signal c              : std_logic_vector(27 downto 0) := "1000000000000000000000000000";
-      signal res 				    : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
-  		signal overflow 		  : std_logic := '0';
-  		constant clk_period 	: time := 10 ns;
+      signal clock          : std_logic := '0';
+      signal reset          : std_logic := '1';
+      signal op1            : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
+      signal op2            : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
+      signal ctrl           : std_logic_vector(27 downto 0) := "0000000000000000000000000000";
+      signal c              : std_logic_vector(27 downto 0) := "1000000000000000000000000000";
+      signal res            : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
+      signal overflow       : std_logic := '0';
+      constant clk_period   : time := 10 ns;
 
-  		begin 
-  			uut: alu port map(
-  					clock => clock,
-  					reset => reset,
-  					op1 => op1,
-  					op2 => op2,
-  					ctrl => ctrl,
-  					res => res,
-  					overflow => overflow
-  				);
+      begin 
+        uut: alu port map(
+            clock => clock,
+            reset => reset,
+            op1 => op1,
+            op2 => op2,
+            ctrl => ctrl,
+            res => res,
+            overflow => overflow
+          );
 
-  			
-  			reset <= '0' after 1 ns;
+        
+        reset <= '0' after 10 ns;
 clk_process:
-  			process
-  			 begin
-  			     clock <= '0';
-  			     wait for clk_period/2;  --for 5 ns signal is '0'.
-  			     clock <= '1';
-  			     wait for clk_period/2;  --for next 5 ns signal is '1'.
-  			end process;
+        process
+         begin
+             clock <= '0';
+             wait for clk_period/2;  --for 5 ns signal is '0'.
+             clock <= '1';
+             wait for clk_period/2;  --for next 5 ns signal is '1'.
+        end process;
 
 monitor:
- 			process
+      process
        variable count_value   : integer := 0;
-       file     out_file      : text open write_mode is "generated_file\out.txt";
+       file     out_file      : text open write_mode is "generated_file/out.txt";
        file   input_file      : text;  --declare input file
        variable line_v        : line;
        variable line_num      : line;
@@ -69,10 +69,10 @@ monitor:
        variable a, b          : string(1 to 32);
        variable num_1         : std_logic_vector (31 downto 0); -- num_1 and num_2 are declared as variable 
        variable num_2         : std_logic_vector (31 downto 0);
- 			 begin 
+       begin 
         while (count_value < c'length) loop
-          file_open(input_file, "sim_input\input.txt", read_mode);
-
+          file_open(input_file, "sim_input/input.txt", read_mode);
+          wait for 5 ns;
           f: loop 
             readline(input_file, line_num);
             read(line_num, a);
@@ -95,8 +95,8 @@ monitor:
                 end if;
               end loop;
 
- 			      op1 			<= num_1;
- 			      op2 			<= num_2;
+            op1       <= num_1;
+            op2       <= num_2;
             ctrl <= std_logic_vector(signed(c) srl count_value);
             wait for 1 ns;
             write(line_v, to_bstring(ctrl)& " " & to_bstring(op1)& " " & to_bstring(op2)& " " & to_bstring(res));
@@ -111,6 +111,6 @@ monitor:
           count_value := count_value + 1;
         end loop; -- end of while loop
         file_close(out_file);
- 			 wait;
- 			end process;
+       wait;
+      end process;
   end;
