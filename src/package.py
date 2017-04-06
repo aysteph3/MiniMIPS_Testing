4d75c4c0edff6f3803a2d66ecd517d6814ecaebe
Siavoosh.Azad <siavoosh.azad@ttu.ee>
2017-04-06 13:22:53 +0300

    added data width to script

--- src/package.py
+++ src/package.py
@@ -328,30 +328,30 @@ related_functions = {
 						"25_28"  	: "01111111111111111111111111111111",
 						"26_28"  	: "01111111111111111111111111111111",
 						"27_28"  	: "01111111111111111111111111111110",
-}	
+}
 
 pre_determinde_patterns = {
 							"F1" : [1, 2, 6, 4, 7],
-							"F2" : [1, 2, 3, 4, 7, 9, 13, 11, 16], 
-							"F3" : [2, 3, 4, 7, 8, 6, 15, 10, 11, 12], 
-							"F4" : [1, 2, 3, 4, 5, 6, 10, 15, 9, 11, 14, 16, 20], 
-							"F5" : [1, 2, 3, 4, 5, 6, 10, 7], 
-							"F6" : [2, 3, 5, 8, 11, 15, 19, 12, 14], 
-							"F7" : [1, 2, 3, 4, 7, 11, 12, 5, 15, 14], 
-							"F8" : [1, 2, 3, 4, 7, 10, 12, 5, 8], 
-							"F9" : [1, 2, 3, 4, 5, 11, 6, 10, 12, 16, 13], 
-							"F10" : [1, 2, 3, 4, 8, 19, 12, 9, 13, 5, 7, 6], 
-							"F11" : [2, 3, 4, 8, 19, 1, 12, 9, 13, 5, 7, 6], 
-							"F12" : [1, 3, 4, 5, 7, 10, 2, 6, 15, 9, 14], 
-							"F13" : [1, 3, 4, 5, 6, 10, 2, 15, 9, 20, 12, 14], 
-							"F14" : [1, 2, 3, 4, 5, 6, 10, 15, 9, 11, 14, 16, 20], 
-							"F15" : [1, 2, 3, 4, 5, 6, 10, 15, 9, 11, 14, 16, 20], 
-							"F16" : [1, 2, 3, 4, 7, 6], 
-} 
+							"F2" : [1, 2, 3, 4, 7, 9, 13, 11, 16],
+							"F3" : [2, 3, 4, 7, 8, 6, 15, 10, 11, 12],
+							"F4" : [1, 2, 3, 4, 5, 6, 10, 15, 9, 11, 14, 16, 20],
+							"F5" : [1, 2, 3, 4, 5, 6, 10, 7],
+							"F6" : [2, 3, 5, 8, 11, 15, 19, 12, 14],
+							"F7" : [1, 2, 3, 4, 7, 11, 12, 5, 15, 14],
+							"F8" : [1, 2, 3, 4, 7, 10, 12, 5, 8],
+							"F9" : [1, 2, 3, 4, 5, 11, 6, 10, 12, 16, 13],
+							"F10" : [1, 2, 3, 4, 8, 19, 12, 9, 13, 5, 7, 6],
+							"F11" : [2, 3, 4, 8, 19, 1, 12, 9, 13, 5, 7, 6],
+							"F12" : [1, 3, 4, 5, 7, 10, 2, 6, 15, 9, 14],
+							"F13" : [1, 3, 4, 5, 6, 10, 2, 15, 9, 20, 12, 14],
+							"F14" : [1, 2, 3, 4, 5, 6, 10, 15, 9, 11, 14, 16, 20],
+							"F15" : [1, 2, 3, 4, 5, 6, 10, 15, 9, 11, 14, 16, 20],
+							"F16" : [1, 2, 3, 4, 7, 6],
+}
 
 
 generated_files_folder = "../generated_files"
-data_width  = 8
+data_width  = 32
 
 def run_scanning_optimization(scanning_test_f1, function_dict, func_id_1, debug, verbose, list_of_necessary_patterns):
 	if scanning_test_f1.count("1") != len(scanning_test_f1):
@@ -391,11 +391,11 @@ def run_scanning_optimization(scanning_test_f1, function_dict, func_id_1, debug,
 						break
 	return scanning_test_f1, list_of_necessary_patterns
 
-	
+
 
 def final_un_used_pattern(number_of_patterns, final_set_of_patterns):
 	"""
-	takes the number of patterns and list of final set of patterns and returns a list of un-used 
+	takes the number of patterns and list of final set of patterns and returns a list of un-used
 	patterns!
 	number_of_patterns: integer: number of patterns in the input pattern file!
 	final_set_of_patterns:  list of integers: list containing all the patterns chosen by the algorithm!
@@ -460,7 +460,7 @@ def find_most_signifacant_scanning(function_dict, function_id_1, current_covered
 		print "\tcurrently covered:", current_covered
 		print "\tcurrently not covered:", not_covered
 	for i in sorted(function_dict.keys()):
-		new_ones =  format(int(not_covered, 2) & int(function_dict[i][function_id_1], 2), 'b').zfill(data_width) 
+		new_ones =  format(int(not_covered, 2) & int(function_dict[i][function_id_1], 2), 'b').zfill(data_width)
 		if new_ones.count("1") in list_of_ones_in_ands.keys():
 			list_of_ones_in_ands[new_ones.count("1")].append(i)
 		else:
@@ -470,14 +470,14 @@ def find_most_signifacant_scanning(function_dict, function_id_1, current_covered
 
 def find_most_signifacant_conformity(function_dict, function_id_1, function_id_2, list_of_used_patterns, list_of_excluded_patterns, current_covered, debug, verbose):
 	"""
-	takes the current state of the covered nodes, i.e. current_covered as a string of binary number 
-	and searches in the patterns in list_of_used_patterns and returns a dictionary with number of 
+	takes the current state of the covered nodes, i.e. current_covered as a string of binary number
+	and searches in the patterns in list_of_used_patterns and returns a dictionary with number of
 	ones as keys and list pattern numbers as value.
-	
+
 	Example:
-			current_covered:    10011101 
+			current_covered:    10011101
 	patterns in list_of_used_patterns:
-	
+
 			pattern no |final and_op for pattern
 			-----------|------------------------
 				1	   |		00001111
@@ -486,7 +486,7 @@ def find_most_signifacant_conformity(function_dict, function_id_1, function_id_2
 				4	   |		01010101
 				5	   |		00110011
 
-	list_of_ones_in_ands = {1: [1, 4], 2:[2, 3, 5]}	
+	list_of_ones_in_ands = {1: [1, 4], 2:[2, 3, 5]}
 	"""
 	list_of_ones_in_ands = {}
 	or_op = "0"*data_width
@@ -503,13 +503,13 @@ def find_most_signifacant_conformity(function_dict, function_id_1, function_id_2
 			if i not in list_of_excluded_patterns:
 				xor_op = format(int(function_dict[i][function_id_1], 2) ^ int(function_dict[i][function_id_2], 2), 'b').zfill(data_width)
 				and_op = format(int(function_dict[i][function_id_2], 2) & int(xor_op, 2), 'b').zfill(data_width)
-				new_ones =  format(int(not_covered, 2) & int(and_op, 2), 'b').zfill(data_width) 
+				new_ones =  format(int(not_covered, 2) & int(and_op, 2), 'b').zfill(data_width)
 				if new_ones.count("1") in list_of_ones_in_ands.keys():
 					list_of_ones_in_ands[new_ones.count("1")].append(i)
 				else:
 					list_of_ones_in_ands[new_ones.count("1")] = [i]
-				or_op = format(int(or_op, 2) | int(and_op, 2), 'b').zfill(data_width)		
-				if debug:		
+				or_op = format(int(or_op, 2) | int(and_op, 2), 'b').zfill(data_width)
+				if debug:
 					print "\t\t"+str(i)+"\t", function_dict[i][0],"\t", function_dict[i][1],"\t", function_dict[i][function_id_1], "\t", function_dict[i][function_id_2], "\t", xor_op, "\t"+str(and_op), "\t"+str(or_op)
 	return list_of_ones_in_ands
 
@@ -560,13 +560,13 @@ def parse_program_arg(arguments, generated_files_folder):
 		print "\n     Copyright (C) 2017 Siavoosh Payandeh Azad, Stephen Oyeniran \n"
 		print "This program optimizes test patterns generation between different functions"
 		print "program arguments:"
-		print "-i [file name]: spcifies the path to the input file" 
-		print "-ot [file name]: spcifies the path to the generated table file" 
-		print "-ost [file name]: spcifies the path to the generated table file for scanning test" 
-		print "-op [file name]: spcifies the path to the generated patterns file" 
-		print "-rfr: redundant function reduction, if used, will use the table in package file to ingore the redundency" 
-		print "-v: makes it more verbose" 
-		print "-dw [data width]: data width of the patterns, default is 8" 
+		print "-i [file name]: spcifies the path to the input file"
+		print "-ot [file name]: spcifies the path to the generated table file"
+		print "-ost [file name]: spcifies the path to the generated table file for scanning test"
+		print "-op [file name]: spcifies the path to the generated patterns file"
+		print "-rfr: redundant function reduction, if used, will use the table in package file to ingore the redundency"
+		print "-v: makes it more verbose"
+		print "-dw [data width]: data width of the patterns, default is 8"
 		print "-debug: enables debug printing"
 		print "---------------------------------------------------------------------------"
 		sys.exit()
@@ -588,7 +588,7 @@ def parse_program_arg(arguments, generated_files_folder):
 		output_table_file_name = generated_files_folder + "/" + arguments[arguments.index('-ot') + 1]
 	else:
 		output_table_file_name = generated_files_folder + "/" + "table.txt"
-		
+
 	if "-op" in arguments[1:]:
 		output_patterns_file_name = generated_files_folder + "/" + arguments[arguments.index('-op') + 1]
 	else:
