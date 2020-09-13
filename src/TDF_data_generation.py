3888f6aaee24b8278e65093d939304fa62aca40f
aysteph3 <aysteph3@yahoo.com>
2020-09-13 16:49:06 +0300

    TDF generation update

--- src/TDF_data_generation.py
+++ src/TDF_data_generation.py
@@ -17,15 +17,16 @@ for number in range(2,lenght_of_line):
     for line in lines:
         data = line.split()
         f_res = format((int(f_res,2) | int(data[number],2)), 'b').zfill(32)
-        #print f_res
+        print f_res
         if f_res == '11111111111111111111111111111111':
             break
-        if prev_res == format(int(data[number],2),'b').zfill(32):
-        #if prev_res == f_res:
-            print prev_res
-            print f_res
-            prev_res = f_res
-            break
+        #if prev_res == format(int(data[number],2),'b').zfill(32):
+        if prev_res != '00000000000000000000000000000000':
+            if f_res == prev_res:
+                #print prev_res
+                #print f_res
+                prev_res = f_res
+                break
     print('\n')
 
 functional_result.close()
